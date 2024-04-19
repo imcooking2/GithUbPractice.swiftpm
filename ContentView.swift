7fcf24c148dcc8fcac2bb1478a0ca9166058e01f
Tomasz <tzuber4496@stu.d214.org>
2024-04-19 11:47:10 -0500

    Created a @State var numberOne for the integer storage

--- ContentView.swift
+++ ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var  numberOne: Int = 0  // var integer setup part one
     var body: some View {
         VStack {
             Image(systemName: "globe")
