11b7774046add88478d23a3757800fb97d2971a8
Bryant Lertpradist <Blertpradist4516@stu.d214.org>
2024-04-19 11:52:42 -0500

    I created var integer part 2

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var  numberOne: Int = 0  // var integer setup part one
+    @State var numberTwo: Int = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
